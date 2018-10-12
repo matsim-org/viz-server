03c28780377ff192b4ad508d3665c50b914f9a7b
Janek Laudan <laudan@tu-berlin.de>
2018-10-12 15:56:31 +0200

    Changed package names from org.matsim.webvis.* -> org.matsim.viz.*

---
+++ files/src/main/java/org/matsim/viz/files/permission/SubjectFactory.java
@@ -0,0 +1,43 @@
+package org.matsim.viz.files.permission;
+
+import lombok.AllArgsConstructor;
+import org.matsim.viz.clientAuth.IntrospectionResult;
+import org.matsim.viz.files.agent.AgentService;
+import org.matsim.viz.files.entities.Agent;
+import org.matsim.viz.files.entities.User;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class SubjectFactory {
+
+    static final String USER = "user-client";
+    static final String PUBLIC_USER = "public-client";
+    static final String SERVICE = "service-client";
+
+    private final AgentService agentService;
+
+    public Optional<Agent> createSubject(IntrospectionResult authResult) {
+
+        switch (authResult.getScope()) {
+            case USER:
+                return Optional.of(findOrCreateUser(authResult.getSub()));
+            case SERVICE:
+                return Optional.of(agentService.getServiceAgent());
+            default:
+                return Optional.of(agentService.getPublicAgent());
+        }
+    }
+
+    public Optional<Agent> createPublicAgent() {
+        return Optional.of(agentService.getPublicAgent());
+    }
+
+    private User findOrCreateUser(String authId) {
+
+        User user = agentService.findByIdentityProviderId(authId);
+        if (user == null)
+            user = agentService.createUser(authId);
+        return user;
+    }
+}
