20a8686af57fb2bfd6d6076795dd52f46f873a18
Janek Laudan <laudan@tu-berlin.de>
2018-10-18 10:57:19 +0200

    * Implement KeyProvider interface for token verification

--- client-auth/src/test/java/org/matsim/viz/clientAuth/OAuthAuthenticatorTest.java
+++ client-auth/src/test/java/org/matsim/viz/clientAuth/OAuthAuthenticatorTest.java
@@ -3,12 +3,11 @@ package org.matsim.viz.clientAuth;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,202 +18,186 @@ import java.net.URI;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
 import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.*;
+import java.util.Date;
+import java.util.Optional;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static junit.framework.TestCase.*;
 
 public class OAuthAuthenticatorTest {
 
-    private static Client jerseyClient = ClientBuilder.newClient();
-    private static ObjectMapper mapper = new ObjectMapper();
-    private static KeyPairGenerator generator;
 
+    private static Client jerseyClient = ClientBuilder.newClient();
+
+    // since authenticator and public key provider are closely entangled we are mocking the id-provider instead of
+    // the key provider
     @Rule
     public WireMockRule wireMockRule = new WireMockRule();
 
+    private URI idProvider;
+    private OAuthAuthenticator<TestPrincipal> testObject;
+
     @BeforeClass
     public static void setUpFixture() throws NoSuchAlgorithmException {
-        generator = KeyPairGenerator.getInstance("RSA");
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         generator.initialize(2048);
     }
 
-    private static OAuthAuthenticator.KeyInformation createKeyInformation(String keyId) {
-        return new OAuthAuthenticator.KeyInformation(
-                keyId, getEncodedPublicKey(), "RS512", "sig", "RSA"
-        );
-    }
-
-    private static String getEncodedPublicKey() {
-        KeyPair keyPair = generator.generateKeyPair();
-        return Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded());
+    @Before
+    public void setUp() {
+        idProvider = URI.create("http://localhost:" + wireMockRule.port());
+        testObject = getAuthenticator();
     }
 
     @Test
-    public void initialize_fetchKeysFromEndpoint() throws JsonProcessingException {
+    public void initialize() {
 
-        // create mock server that returns keys
-        List<OAuthAuthenticator.KeyInformation> keyInformations = new ArrayList<>();
-        keyInformations.add(createKeyInformation("my-key"));
-        wireMockRule.stubFor(get(WireMock.anyUrl())
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(mapper.writeValueAsString(keyInformations))
-                ));
-
-        URI idProvider = URI.create("http://localhost:" + wireMockRule.port());
-
-        OAuthAuthenticator<Principal> authenticator =
-                new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new Principal()));
+        OAuthAuthenticator<TestPrincipal> authenticator =
+                new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new TestPrincipal()));
 
         assertNotNull(authenticator);
-        verify(getRequestedFor(urlEqualTo("/certificates")));
-    }
-
-    @Test
-    public void initialize_cannotReadFetchedKey() throws JsonProcessingException {
-
-        // create mock server that returns keys
-        List<OAuthAuthenticator.KeyInformation> keyInformations = new ArrayList<>();
-        keyInformations.add(new OAuthAuthenticator.KeyInformation(
-                "my-key", "invalid-rsa-key", "RS512", "sig", "RSA"
-        ));
-
-        wireMockRule.stubFor(get(WireMock.anyUrl())
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(mapper.writeValueAsString(keyInformations))
-                ));
-
-        URI idProvider = URI.create("http://localhost:" + wireMockRule.port());
-        OAuthAuthenticator<Principal> authenticator =
-                new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new Principal()));
-
-        assertNotNull(authenticator);
-        verify(getRequestedFor(urlEqualTo("/certificates")));
-    }
-
-    @Test
-    public void initialize_unknownKeyFormat() throws JsonProcessingException {
-
-        // create mock server that returns keys
-        List<OAuthAuthenticator.KeyInformation> keyInformations = new ArrayList<>();
-        keyInformations.add(new OAuthAuthenticator.KeyInformation(
-                "my-key", getEncodedPublicKey(), "RS512", "sig", "DSA"
-        ));
-
-        wireMockRule.stubFor(get(WireMock.anyUrl())
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(mapper.writeValueAsString(keyInformations))
-                ));
-
-        URI idProvider = URI.create("http://localhost:" + wireMockRule.port());
-        OAuthAuthenticator<Principal> authenticator =
-                new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new Principal()));
-
-        assertNotNull(authenticator);
-        verify(getRequestedFor(urlEqualTo("/certificates")));
     }
 
     @Test
     public void authenticate_malformedToken_emptyOptional() throws JsonProcessingException {
 
-        final String token = "invalid-jwt";
-        final URI idProvider = URI.create("http://localhost:" + wireMockRule.port());
+        final String keyId = "my-key-id";
+        TestUtils.mockCertificatesEndpoint(wireMockRule, new PublicKeyProvider.KeyInformation[]{TestUtils.createKeyInformation(keyId)});
+        final String malformedToken = "some-token";
 
-        List<OAuthAuthenticator.KeyInformation> keyInformations = new ArrayList<>();
-        keyInformations.add(createKeyInformation("my-key"));
-        wireMockRule.stubFor(get(WireMock.anyUrl())
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(mapper.writeValueAsString(keyInformations))
-                ));
-        OAuthAuthenticator<Principal> authenticator =
-                new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new Principal()));
-
-        Optional<Principal> result = authenticator.authenticate(token);
+        Optional<TestPrincipal> result = testObject.authenticate(malformedToken);
 
         assertFalse(result.isPresent());
     }
 
     @Test
-    public void authenticate_unknownKey_newKeysAreFetched() throws JsonProcessingException {
+    public void authenticate_expiredToken_emptyOptional() throws JsonProcessingException {
 
-        final URI idProvider = URI.create("http://localhost:" + wireMockRule.port());
-        final String keyId = "my-key";
-        final String subject = "some-user-id";
-        final KeyPair keyPair = generator.generateKeyPair();
-        final RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        final RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        final KeyPair keyPair = TestUtils.createRSAKeyPair();
+        final String keyId = "my-key-id";
+        final String subject = "some-subject-id";
+        final PublicKeyProvider.KeyInformation[] keys = new PublicKeyProvider.KeyInformation[]{
+                TestUtils.createKeyInformation(keyId, keyPair.getPublic())
+        };
+        final String token = JWT.create()
+                .withSubject(subject)
+                .withExpiresAt(Date.from(Instant.now().minus(Duration.ofHours(1))))
+                .withKeyId(keyId)
+                .withIssuer(idProvider.toString())
+                .sign(Algorithm.RSA512(null, (RSAPrivateKey) keyPair.getPrivate()));
+        TestUtils.mockCertificatesEndpoint(wireMockRule, keys);
+
+        Optional<TestPrincipal> result = testObject.authenticate(token);
+
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void authenticate_unknownIssuerInToken_emptyOptional() throws JsonProcessingException {
+
+        final KeyPair keyPair = TestUtils.createRSAKeyPair();
+        final String keyId = "my-key-id";
+        final String subject = "some-subject-id";
+        final PublicKeyProvider.KeyInformation[] keys = new PublicKeyProvider.KeyInformation[]{
+                TestUtils.createKeyInformation(keyId, keyPair.getPublic())
+        };
+        final String token = JWT.create()
+                .withSubject(subject)
+                .withExpiresAt(Date.from(Instant.now().plus(Duration.ofHours(1))))
+                .withKeyId(keyId)
+                .withIssuer(URI.create("https://unknown-issuer.com").toString())
+                .sign(Algorithm.RSA512(null, (RSAPrivateKey) keyPair.getPrivate()));
+        TestUtils.mockCertificatesEndpoint(wireMockRule, keys);
+
+        Optional<TestPrincipal> result = testObject.authenticate(token);
+
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void authenticate_cantFetchKeyId_emptyOptional() throws JsonProcessingException {
+
+        final KeyPair keyPair = TestUtils.createRSAKeyPair();
+        final String keyId = "my-key-id";
+        final String subject = "some-subject-id";
+        final PublicKeyProvider.KeyInformation[] keys = new PublicKeyProvider.KeyInformation[]{
+                TestUtils.createKeyInformation(keyId, keyPair.getPublic())
+        };
+        final String token = JWT.create()
+                .withSubject(subject)
+                .withExpiresAt(Date.from(Instant.now().plus(Duration.ofHours(1))))
+                .withKeyId("some-other-key-id")
+                .withIssuer(idProvider.toString())
+                .sign(Algorithm.RSA512(null, (RSAPrivateKey) keyPair.getPrivate()));
+        TestUtils.mockCertificatesEndpoint(wireMockRule, keys);
+
+        Optional<TestPrincipal> result = testObject.authenticate(token);
+
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void authenticate_principalProviderException_emptyOptional() throws JsonProcessingException {
+
+        final KeyPair keyPair = TestUtils.createRSAKeyPair();
+        final String keyId = "my-key-id";
+        final String subject = "some-subject-id";
+        final PublicKeyProvider.KeyInformation[] keys = new PublicKeyProvider.KeyInformation[]{
+                TestUtils.createKeyInformation(keyId, keyPair.getPublic())
+        };
         final String token = JWT.create()
                 .withSubject(subject)
                 .withExpiresAt(Date.from(Instant.now().plus(Duration.ofHours(1))))
                 .withKeyId(keyId)
                 .withIssuer(idProvider.toString())
-                .sign(Algorithm.RSA512(publicKey, privateKey));
+                .sign(Algorithm.RSA512(null, (RSAPrivateKey) keyPair.getPrivate()));
+        TestUtils.mockCertificatesEndpoint(wireMockRule, keys);
 
-        List<OAuthAuthenticator.KeyInformation> keyInformations = new ArrayList<>();
-        keyInformations.add(new OAuthAuthenticator.KeyInformation(
-                "my-key", Base64.getEncoder().encodeToString(publicKey.getEncoded()), "RS512", "sig", "RSA"
-        ));
-        wireMockRule.stubFor(get(WireMock.anyUrl())
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(mapper.writeValueAsString(keyInformations))
-                ));
+        testObject = new OAuthAuthenticator<>(jerseyClient, idProvider, result -> {
+            throw new RuntimeException("something went wrong");
+        });
 
-        OAuthAuthenticator<Principal> authenticator =
-                new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new Principal(result.getSubjectId())));
+        Optional<TestPrincipal> result = testObject.authenticate(token);
 
-        Optional<Principal> result = authenticator.authenticate(token);
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void authenticate_allGood_principal() throws JsonProcessingException {
+
+        final KeyPair keyPair = TestUtils.createRSAKeyPair();
+        final String keyId = "my-key-id";
+        final String subject = "some-subject-id";
+        final PublicKeyProvider.KeyInformation[] keys = new PublicKeyProvider.KeyInformation[]{
+                TestUtils.createKeyInformation(keyId, keyPair.getPublic())
+        };
+        final String token = JWT.create()
+                .withSubject(subject)
+                .withExpiresAt(Date.from(Instant.now().plus(Duration.ofHours(1))))
+                .withKeyId(keyId)
+                .withIssuer(idProvider.toString())
+                .sign(Algorithm.RSA512(null, (RSAPrivateKey) keyPair.getPrivate()));
+        TestUtils.mockCertificatesEndpoint(wireMockRule, keys);
+
+        testObject = new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new TestPrincipal(result.getSubjectId())));
+
+        Optional<TestPrincipal> result = testObject.authenticate(token);
 
         assertTrue(result.isPresent());
         assertEquals(subject, result.get().getName());
     }
 
-    @Test
-    public void authenticate_unknownKey_newKeysAreFetchedButKeyIsNotThere_emptyOptional() throws JsonProcessingException {
-
-        final URI idProvider = URI.create("http://localhost:" + wireMockRule.port());
-        final String keyId = "my-key";
-        final String subject = "some-user-id";
-        final KeyPair keyPair = generator.generateKeyPair();
-        final RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        final RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        final String token = JWT.create()
-                .withSubject(subject)
-                .withExpiresAt(Date.from(Instant.now().plus(Duration.ofHours(1))))
-                .withKeyId(keyId)
-                .withIssuer(idProvider.toString())
-                .sign(Algorithm.RSA512(publicKey, privateKey));
-
-        List<OAuthAuthenticator.KeyInformation> keyInformations = new ArrayList<>();
-        keyInformations.add(new OAuthAuthenticator.KeyInformation(
-                "other-key-id", Base64.getEncoder().encodeToString(publicKey.getEncoded()), "RS512", "sig", "RSA"
-        ));
-        wireMockRule.stubFor(get(WireMock.anyUrl())
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(mapper.writeValueAsString(keyInformations))
-                ));
-
-        OAuthAuthenticator<Principal> authenticator =
-                new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new Principal(result.getSubjectId())));
-
-        Optional<Principal> result = authenticator.authenticate(token);
-
-        assertFalse(result.isPresent());
+    private OAuthAuthenticator<TestPrincipal> getAuthenticator() {
+        return new OAuthAuthenticator<>(jerseyClient, idProvider, result -> Optional.of(new TestPrincipal()));
     }
 
-    @Getter
     @AllArgsConstructor
     @NoArgsConstructor
-    private static class Principal implements java.security.Principal {
+    @Getter
+    private static class TestPrincipal implements Principal {
 
         private String name;
     }
