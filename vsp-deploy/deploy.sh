196cf029708145b8af73b57cb11af4f7c1ac839c
Jakub Wilk <jwilk79@users.noreply.github.com>
2019-02-27 21:53:58 +0100

    Create deploy.sh

---
+++ vsp-deploy/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# VSP deploy script for travis CI
+DOMAIN=$1
+echo "domain is: $DOMAIN"
+# check whether ssh-agent is present
+eval $(ssh-agent -s)
+# see bug with line endings at https://gitlab.com/gitlab-examples/ssh-private-key/issues/1
+# this works but needs a base64 encoded key with cmd "cat id_rsa | base64 -w0"
+# paste the output of cmd into var VSP_SSH_KEY_BASE64 in double-quotes (var in Travis)
+ssh-add <(echo "VSP_SSHKEY_VIZDEPLOY_CNODE00_BASE64"| base64 --decode)
+# remove old webfiles
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/$DOMAIN/app/*"
+# upload fresh built files
+scp -r dist/* vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/$DOMAIN/app/
+# build new docker image 
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_viz-auth-play.sh"
