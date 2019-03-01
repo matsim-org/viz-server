2b94df7fbe9a1feb85538c561cd0b3889f8d7842
Janek Laudan <laudan@tu-berlin.de>
2019-03-01 10:56:18 +0100

    Add vsp-deploy for files and auth component

--- vsp-deploy/deploy.sh
+++ vsp-deploy/deploy.sh
@@ -9,8 +9,11 @@ eval $(ssh-agent -s)
 # paste the output of cmd into var VSP_SSH_KEY_BASE64 in double-quotes (var in Travis)
 ssh-add <(echo "VSP_SSHKEY_VIZDEPLOY_CNODE00_BASE64"| base64 --decode)
 # remove old webfiles
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/$DOMAIN/app/*"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/viz-auth-$DOMAIN/app/*"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/viz-files-$DOMAIN/app/*"
 # upload fresh built files
-scp -r dist/* vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/$DOMAIN/app/
+scp -r auth/target/auth-1.0-SNAPSHOT.jar vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/viz-auth-$DOMAIN/app/
+scp -r files/target/files-1.0-SNAPSHOT.jar vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/viz-files-$DOMAIN/app/
 # build new docker image 
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_$DOMAIN.sh"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_viz-auth-$DOMAIN.sh"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_viz-auth-$DOMAIN.sh"
