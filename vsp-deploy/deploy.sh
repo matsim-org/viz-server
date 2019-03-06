32f250bfbc21f268a0abf481461cf3f06c6c1524
Janek Laudan <laudan@tu-berlin.de>
2019-03-06 09:57:50 +0100

    * remove aws deploy * set up internal and stable deploy for vsp-deploy

--- vsp-deploy/deploy.sh
+++ vsp-deploy/deploy.sh
@@ -9,14 +9,14 @@ eval $(ssh-agent -s)
 # paste the output of cmd into var VSP_SSH_KEY_BASE64 in double-quotes (var in Travis)
 ssh-add <(echo "$VSP_SSHKEY_VIZDEPLOY_CNODE00_BASE64"| base64 --decode)
 # remove old webfiles
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/viz-auth-$DOMAIN/app/*"
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/viz-files-$DOMAIN/app/*"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/viz-auth$DOMAIN/app/*"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "rm -r /srv/www/viz-files$DOMAIN/app/*"
 # upload fresh built files
-scp -r auth/target/auth-1.0-SNAPSHOT.jar vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/viz-auth-$DOMAIN/app/
-scp -r files/target/files-1.0-SNAPSHOT.jar vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/viz-files-$DOMAIN/app/
+scp -r auth/target/auth-1.0-SNAPSHOT.jar vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/viz-auth$DOMAIN/app/
+scp -r files/target/files-1.0-SNAPSHOT.jar vizdeploy@cnode00.vsp.tu-berlin.de:/srv/www/viz-files$DOMAIN/app/
 # set correct permissions
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "chmod 0664 /srv/www/viz-auth-$DOMAIN/app/auth-1.0-SNAPSHOT.jar"
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "chmod 0664 /srv/www/viz-files-$DOMAIN/app/files-1.0-SNAPSHOT.jar"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "chmod 0664 /srv/www/viz-auth$DOMAIN/app/auth-1.0-SNAPSHOT.jar"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "chmod 0664 /srv/www/viz-files$DOMAIN/app/files-1.0-SNAPSHOT.jar"
 # build new docker image 
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_viz-auth-$DOMAIN.sh"
-ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_viz-files-$DOMAIN.sh"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_viz-auth$DOMAIN.sh"
+ssh vizdeploy@cnode00.vsp.tu-berlin.de "sudo -u dockerbuilder /home/vizdeploy/bin/build_viz-files$DOMAIN.sh"
